3d6686e0b56548c8d17fd1a0eda2c3a96bc45fb6
NicMilli <nicbass2@gmail.com>
2023-02-06 17:25:16 -0800

    Initial commit - passing bare minimum

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,77 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var elementsWithClassName = [];
+  var body = document.body;
+
+  var checkChildNodes = function(node) {
+    var nodeClassList = node.classList;
+    nodeChildNodes = node.childNodes;
+    if (nodeClassList && nodeClassList.contains(className)) {
+      elementsWithClassName.push(node);
+    }
+    if (nodeChildNodes) {
+      nodeChildNodes.forEach(function(node) {
+        checkChildNodes(node);
+      });
+    }
+  };
+
+  checkChildNodes(body);
+  return elementsWithClassName;
 };
+// var getElementsByClassName = function(className, node) {
+//   //parameters: string of class names to match, multiple classnames separated by white space
+//   var classNames = className.split(' ');
+//   //Loop through document body and collect all elements with the target classname
+//   var elementsWithClassname = [];
+//   if (!node && document.body) {
+//     var body = document.body;
+
+//     if (body.classList.contains(className)) {
+//       elementsWithClassname.concat(body);
+//     }
+//     var bodyChildNodes = body.childNodes;
+//     bodyChildNodes.forEach(function(element) {
+//       console.log(element)
+//       elementsWithClassname.concat(getElementsByClassName(className, element));
+//     });
+//   } else {
+//     console.log(node)
+//     if (!node) {
+//       return [];
+//     }
+//     if (node.classList && node.classList.contains(className)) {
+//       return [node];
+//     } else {
+//       return [];
+//     }
+//   }
+
+
+
+//   //   if (node.childNodes.length === 0) {
+
+//   //       return elementsWithClassname.concat(node);
+//   //     }
+//   //     console.log(node.childNodes);
+//   //     return elementsWithClassname.concat(node, getElementsByClassName(className, node.children));
+//   //   }
+
+//   //   if (node.childNodes.length > 0) {
+//   //     return elementsWithClassname.concat(getElementsByClassName(className, node.children));
+//   //   } else {
+//   //     return [];
+//   //   }
+//   // } else {
+
+//   // }
+//   // create function to check whether the target node has the required classname
+//   //and push to elementsWithClassName
+
+//   //return a HTML collection of found elements
+//   return elementsWithClassname;
+// };
+
+console.log(getElementsByClassName('<div><div class="targetClassName"><div class="targetClassName"></div></div></div>'));
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,61 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (typeof(obj) === 'function') {
+    return undefined;
+  } else if (obj === undefined) {
+    return undefined;
+  }
+
+  var outputString = '';
+  if (typeof(obj) === 'object') {
+    //nested case
+    if (Array.isArray(obj)) {
+      //Array case
+      if (obj.length === 0) {
+        return '[]';
+      }
+      outputString += '[';
+      for (let i = 0; i < obj.length; i++) {
+        if (i === obj.length - 1) {
+          outputString += stringifyJSON(obj[i]) + ']';
+        } else {
+          outputString += stringifyJSON(obj[i]) + ',';
+        }
+      }
+    } else {
+      //Object literal case
+      var keys = Object.keys(obj);
+      if (Object.keys(obj).length === 0) {
+        return '{}';
+      }
+      outputString += '{';
+      for (let j = 0; j < keys.length; j++) {
+        console.log(keys);
+        let key = keys[j];
+        let val = stringifyJSON(obj[key]);
+        if (val === undefined) {
+          return '{}';
+        }
+        if (!keys[j + 1]) {
+          outputString += stringifyJSON(key) + ':' + val + '}';
+        } else {
+          outputString += stringifyJSON(key) + ':' + val + ',';
+        }
+      }
+
+    }
+  } else {
+    //single value case
+    if (typeof(obj) === 'string') {
+      return '"' + obj + '"';
+    } else {
+      return obj.toString();
+    }
+  }
+
+
+  return outputString;
 };
